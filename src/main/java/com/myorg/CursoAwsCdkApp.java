7464a9d005755c6abf5f012701b6f4b76e245be0
pedro <developerpedropbazzo@gmail.com>
2021-08-05 23:48:49 -0300

    first commit

---
+++ src/main/java/com/myorg/CursoAwsCdkApp.java
@@ -0,0 +1,25 @@
+package com.myorg;
+
+import software.amazon.awscdk.core.App;
+
+import java.util.Arrays;
+
+public class CursoAwsCdkApp {
+    public static void main(final String[] args) {
+        App app = new App();
+
+        VpcStack vpcStack = new VpcStack(app, "Vpc");
+
+        ClusterStack clusterStack = new ClusterStack(app, "Cluster", vpcStack.getVpc());
+        clusterStack.addDependency(vpcStack);
+
+        RdsStack rdsStack = new RdsStack(app, "Rds", vpcStack.getVpc());
+        rdsStack.addDependency(vpcStack);
+
+        Service01Stack service01Stack = new Service01Stack(app, "Service01", clusterStack.getCluster());
+        service01Stack.addDependency(clusterStack);
+        service01Stack.addDependency(rdsStack);
+
+        app.synth();
+    }
+}
